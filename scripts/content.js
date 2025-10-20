3c0d1040b68df01e5c61ca53f2d4d16a951cd16d
moses-codes <moses.ilagan@gmail.com>
2025-10-20 12:02:39 -0700

    content script updated

---
+++ scripts/content.js
@@ -0,0 +1,9 @@
+function insertFlag(textarea) {
+  console.log("this is a content file");
+  if (!textarea) {
+    return;
+  }
+}
+
+insertFlag(document.querySelector("textarea"));
+insertFlag(document.querySelector("textarea"));
