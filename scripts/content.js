fa2f9502ed82e2f5254cb25cdefdfee3929e11b3
moses-codes <moses.ilagan@gmail.com>
2025-10-27 16:06:25 -0700

    set up content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,20 @@
-alert("this is one of the sites");
+// alert("this is one of the sites");
 
-console.log(window.location.href);
+const initialLocation = window.location.href;
+const modified = initialLocation.split("search?q=")[1].split("&")[0];
+console.log(modified.split("+"));
+if (modified.split("+").includes("-ai")) {
+  console.log("this is already an -ai search.");
+} else {
+  const newQuery = modified + "+-ai";
+  const newHref = initialLocation.split(modified).join(newQuery);
+  console.log(initialLocation, modified, newQuery, newHref);
+  window.location.href = newHref;
+}
+
+const form = document.getElementById("tsf");
+console.log(form);
+
+form.addEventListener("submit", (e) => {}), { capture: true };
+
+// window.location.href = newHref;
